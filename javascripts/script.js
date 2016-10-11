83e8cf8e49fe70a430cea1616cbf1dabc2364e0b
Chandler Kelley <chandler.t.kelley@gmail.com>
2016-10-11 18:24:28 -0400

    Changed media scrolloe-over color, removed title graphic, and made royce bigger

--- javascripts/script.js
+++ javascripts/script.js
@@ -4,32 +4,33 @@ $(function() {
 	var $media = $(".media");
 	var $places = $(".places");
 	var $about = $(".about");
-	var $kawan = $(".kawan");
+	var $royce = $(".royce");
 	var $center = $(".pic-container")
 
 	$body.css("background-color", "#F88868");
 
 	$center.hover(function() {
-		$kawan.css("object-position", "0 0");
+		$royce.css("object-position", "0 0");
 	})
 
 	$journal.hover(function() {
 		$body.css("background-color", "#FFE36A");
-		$kawan.css("object-position", "-300px 0");
+		$royce.css("object-position", "-450px 0");
 	})
 
 	$media.hover(function() {
-		$body.css("background-color", "#F878A8");
-		$kawan.css("object-position", "-600px 0");
+		// $body.css("background-color", "#F878A8");
+		$body.css("background-color", "#BED3F6");
+		$royce.css("object-position", "-900px 0");
 	})
 
 	$places.hover(function() {
 		$body.css("background-color", "#4ccede");
-		$kawan.css("object-position", "0 -400px");
+		$royce.css("object-position", "0 -600px");
 	})
 
 	$about.hover(function() {
 		$body.css("background-color", "#F88868");
-		$kawan.css("object-position", "-300px -400px");
+		$royce.css("object-position", "-450px -600px");
 	})
 })
