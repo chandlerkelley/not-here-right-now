07532aa5a3ccb72fc1ef04eb2776582f5f9dcc9a
Chandler Kelley <chandler.t.kelley@gmail.com>
2016-11-12 12:45:00 -0500

    Added head and body sprites, added bounce animation

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,36 +1,54 @@
 $(function() {
 	var $body = $("body");
-	var $footage = $(".footage");
-	var $media = $(".media");
-	var $places = $(".places");
-	var $about = $(".about");
-	var $royce = $(".royce");
 	var $center = $(".pic-container")
+	var $bodies = $("#bodies");
+	var $heads = $("#heads");
+	var $about = $(".about");
+	var $places = $(".places");
+	var $footage = $(".footage");
+	var $journal = $(".journal");
+	var $teaser = $(".teaser");
+	var $tourinfo = $(".tour-info");
 
 	$body.css("background-color", "#F88868");
 
 	$center.hover(function() {
-		$royce.css("object-position", "0 0");
-	})
-
-	$footage.hover(function() {
-		$body.css("background-color", "#FFE36A");
-		$royce.css("object-position", "-51vh 0");
-	})
-
-	$media.hover(function() {
-		// $body.css("background-color", "#F878A8");
-		$body.css("background-color", "#BED3F6");
-		$royce.css("object-position", "-102vh 0");
-	})
-
-	$places.hover(function() {
-		$body.css("background-color", "#4ccede");
-		$royce.css("object-position", "0 -68vh");
+		$bodies.css("object-position", "0");
+		$heads.css("object-position", "0");
 	})
 
 	$about.hover(function() {
 		$body.css("background-color", "#F88868");
-		$royce.css("object-position", "-51vh -68vh");
+		$bodies.css("object-position", "-51vh");
+		$heads.css("object-position", "-51vh");
+	})
+
+	$places.hover(function() {
+		$body.css("background-color", "#4ccede");
+		$bodies.css("object-position", "-102vh");
+		$heads.css("object-position", "-102vh");
+	})
+
+	$footage.hover(function() {
+		$body.css("background-color", "#FFE36A");
+		$bodies.css("object-position", "-153vh");
+		$heads.css("object-position", "-153vh");
+	})
+
+	$journal.hover(function() {
+		// $body.css("background-color", "#F878A8");
+		$body.css("background-color", "#4ccede");
+		$bodies.css("object-position", "-204vh");
+		$heads.css("object-position", "-204vh");
+	})
+
+	$teaser.hover(function() {
+		$body.css("background-color", "#FFE36A");
+
+	})
+
+	$tourinfo.hover(function() {
+		$body.css("background-color", "#F88868");
+
 	})
 })
