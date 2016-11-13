ee893b932d0be6906a37a5403e8bbe18658e2601
Chandler Kelley <chandler.t.kelley@gmail.com>
2016-11-13 18:11:29 -0500

    Random video and copy text pops up on footage page

--- javascripts/script.js
+++ javascripts/script.js
@@ -62,4 +62,16 @@ $(function() {
 			$(this).children(".speech-bubble").css("display", "inline-block");
 		}
 	})
+
+	var videos = [
+					{"embed": "G4OpVQsxCmc", "copy": "This priest sits in one of the most beautiful temples in the world and blesses people all day. I still wear my red bracelet to this day."},
+					{"embed": "T7DQIGcj1MQ", "copy": "The children in Cambodia are incredibly vibrant. I got the eeriest feeling that they were supposed to be doing work at this time though."},
+					{"embed": "TLEbJBUQmes", "copy": "Brothers from other mothers. My Swiss homie, Michel, traveled with me for a couple weeks through Cambodia and Thailand. #FriendsofNHRN"},
+					{"embed": "ckQAiVcGqY8", "copy": "Entering the gates of the Angkor Temple Complex. This place was insane."}
+				]
+
+	var vidIndex = Math.floor(Math.random() * 4);
+	$(".footage-container").append('<iframe width="560" height="315" src="https://www.youtube.com/embed/' + videos[vidIndex].embed + '?controls=0" frameborder="0" allowfullscreen></iframe>');
+	console.log(vidIndex);
+	$(".video-copy").append('<p>' + videos[vidIndex].copy + '</p>')
 })
